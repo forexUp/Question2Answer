f6be9b3d350e0ac0fa83804def521f63ea4d06e6
forexUp <36788649+forexUp@users.noreply.github.com>
2018-02-24 18:48:20 +0100

    Create qa-lang-profile.php

---
+++ Q2A_1.8/qa-lang/it/qa-lang-profile.php
@@ -0,0 +1,107 @@
+<?php
+	
+/*
+	Question2Answer 1.2.1 (c) 2010, Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-profile.php
+	Version: 1.2.1
+	Date: 2010-07-29 03:54:35 GMT
+	Description: Language phrases for user profile page
+
+
+	This software is free to use and modify for public websites, so long as a
+	link to http://www.question2answer.org/ is displayed on each page. It may
+	not be redistributed or resold, nor may any works derived from it.
+	
+	More about this license: http://www.question2answer.org/license.php
+
+
+	THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+	THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+	return array(
+		'1_chosen_as_best' => ' (1 scelta come migliore)',
+		'1_down_vote' => '1 votato negativamente',
+		'1_up_vote' => '1 votato positivamente',
+		'1_with_best_chosen' => ' (1 con scelta la risposta migliore)',
+		'activity_by_x' => 'Attività da ^',
+		'answers' => 'Risposte:',
+		'comments' => 'Commenti:',
+		'gave_out' => 'Emessi:',
+		'my_account_title' => 'Dettagli del mio account',
+		'questions' => 'Quesiti:',
+		'ranked_x' => ' (posizione in classifica #^)',
+		'received' => 'Ricevuto:',
+		'score' => 'Punteggio:',
+		'delete_pm_popup' => 'Elimina questo messaggio privato',
+		'user_x' => 'Utente ^',
+		'voted_on' => 'Votato il:',
+		'x_chosen_as_best' => ' (^ scelte come migliori)',
+		'x_down_votes' => '^ voti negativi',
+		'x_up_votes' => '^ voti positivi',
+		'x_with_best_chosen' => ' (^ con scelte la risposte migliori)',
+		'bonus_points' => "Punti bonus:",
+		'extra_privileges' => "Privilegi extra:",
+		'no_posts_by_x' => "Nessun messaggio da ^",
+		'permit_anon_view_ips' => "Visualizzare indirizzi IP messaggi anonimi",
+		'permit_close_q' => "Chiudere una domanda",
+		'permit_delete_hidden' => "Cancellare messaggi nascosti",
+		'permit_edit_a' => "Modificare una risposta",
+		'permit_edit_c' => "Modificare un commento",
+		'permit_edit_q' => "Modificare una domanda",
+		'permit_flag' => "Selezionare messaggi",
+		'permit_hide_show' => "Nascondere o pubblicare messaggi",
+		'permit_moderate' => "Approvare o rifiutare messaggi",
+		'permit_post_a' => "Rispondere a una domanda",
+		'permit_post_c' => "Aggiungere un commento",
+		'permit_post_q' => "Fare una domanda",
+		'permit_recat' => "Modificare la categoria a una domanda",
+		'permit_retag' => "Ritaggare una domanda",
+		'permit_select_a' => "Selezionare risposte per una domanda",
+		'permit_view_q_page' => "Visualizzare la pagina delle domande",
+		'permit_vote_a' => "Voti alle risposte",
+		'permit_vote_down' => "Voti negativi ai messaggi",
+		'permit_vote_q' => "Voti alle domande",
+		'recent_activity_by_x' => "Attività recenti di ^",
+		'send_private_message' => " - ^1invia messaggio privato^2",
+		'set_bonus_button' => "Aggiorna bonus",
+		'title' => "Livello:",
+		'answers_by_x' => 'Risposte da ^',			
+		'delete_wall_post_popup' => 'Cancella questo messaggio dalla bacheca',	
+		'no_answers_by_x' => 'Nessuna risposta da ^',
+		'no_questions_by_x' => 'Nessuna domanda da ^',		
+		'permit_edit_silent' => 'Modifica messaggi senza notificare',		
+		'permit_post_wall' => 'Scrivi sulla bacheca degli utenti',	
+		'permit_view_voters_flaggers' => 'Visulizza chi ha votato o segnalato il messaggio',
+		'post_wall_blocked' => 'Questo utente ha disabilitato i nuovi messaggi sulla sua bacheca',	
+		'post_wall_button' => 'Aggiungi un messaggio in bacheca',		
+		'post_wall_empty' => 'Inserisci un messaggio in questa bacheca',	
+		'post_wall_limit' => 'Non puoi inserire ulteriori messaggi in bacheca in quest\'ora',
+		'post_wall_must_be_approved' => 'Il tuo account deve essere approvato per poter scrivere in bacheca. Attendi o ^1aggiungi maggiori informazioni^2.',
+		'post_wall_must_confirm' => '^5conferma il tuo indirizzo email^6 per scrivere in bacheca.',
+		'post_wall_must_login' => '^1log in^2 o ^3registrati^4 per scrivere un messaggio in bacheca.',
+		'questions_by_x' => 'Domande di ^',		
+		'wall_for_x' => 'Bacheca di ^',
+		'permit_view_new_users_page' => "Visualizza la pagina dei nuovi utenti",
+		'permit_view_special_users_page' => "Visualizza la pagina degli utenti speciali",
+		'permit_vote_c' => "Voti sui commenti",
+		'user_x_disabled_pms' => "L'utente ^ ha disabilitato i messaggi privati.",		
+		'wall_view_more' => 'Visualizza altri messaggi in bacheca...'
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
