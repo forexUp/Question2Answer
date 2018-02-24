74c1b9826090771e6e8c067b4979928099ac2750
forexUp <36788649+forexUp@users.noreply.github.com>
2018-02-24 18:48:43 +0100

    Create qa-lang-question.php

---
+++ Q2A_1.8/qa-lang/it/qa-lang-question.php
@@ -0,0 +1,192 @@
+<?php
+	
+/*
+	Question2Answer 1.2.1 (c) 2010, Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-question.php
+	Version: 1.2.1
+	Date: 2010-07-29 03:54:35 GMT
+	Description: Language phrases for question page
+
+
+	This software is free to use and modify for public websites, so long as a
+	link to http://www.question2answer.org/ is displayed on each page. It may
+	not be redistributed or resold, nor may any works derived from it.
+	
+	More about this license: http://www.question2answer.org/license.php
+
+
+	THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+	THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+	return array(
+		'1_answer_title' => '1 Risposta',
+		'a_convert_to_c_on' => 'Converti questa risposta in un commento su:',
+		'a_convert_to_c' => 'Converti questa risposta in un commento',
+		'a_convert_warn_cs' => 'Attenzione: Questa conversione non potrà essere annullata e sposterà tutti commenti di questa risposta.',
+		'a_convert_warn' => 'Attenzione: Questa conversione non potrà essere annullata.',
+		'a_notify_email' => 'Inviami una email al seguente indirizzo se la mia risposta viene selezionata o commentata:',
+		'a_notify_label' => 'Inviami una email al mio indirizzo se la mia risposta viene selezionata o commentata',
+		'a_notify_x_label' => 'Inviami una email (^) se la mia risposta viene selezionata o commentata',
+		'add_answer_button' => 'Aggiungi una risposta',
+		'add_comment_button' => 'Aggiungi un commento',
+		'answer_button' => 'rispondi',
+		'answer_limit' => 'Troppe risposte ricevute - riprova tra un\'ora',
+		'answer_must_confirm' => '^5Conferma il tuo indirizzo email^6 per rispondere al quesito.',
+		'answer_must_login' => 'Fai il ^1log in^2 oppure ^3registrati^4 per rispondere al quesito.',
+		'answer_q_popup' => 'Rispondi a questa domanda',
+		'ask_button' => 'Nuova domanda',
+		'ask_follow_from_a' => 'Il tuo quesito sarà correlato a questa risposta:',
+		'ask_follow_title' => 'crea domanda',
+		'ask_limit' => 'Troppi quesiti ricevuti - riprova tra un\'ora',
+		'ask_must_confirm' => '^5Conferma il tuo indirizzo email^6 per porre un quesito.',
+		'ask_must_login' => 'Fai il ^1log in^2 oppure ^3registrati^4 per porre il quesito.',
+		'ask_same_q' => 'Prima di procedere, controlla che il tuo quesito non è stato già posto:',
+		'ask_title' => 'crea domanda',
+		'c_notify_email' => 'Inviami una email al seguente indirizzo se un commento viene aggiunto dopo il mio:',
+		'c_notify_label' => 'Inviami una email se un commento viene aggiunto dopo il mio',
+		'c_notify_x_label' => 'Inviami una email (^) se un commento viene aggiunto dopo il mio',
+		'claim_button' => 'Ho scritto questo:',
+		'comment_a_popup' => 'Aggiungi un commento a questa risposta',
+		'comment_button' => 'commenta',
+		'comment_limit' => 'Troppi commenti ricevuti - riprova tra un\'ora',
+		'comment_must_confirm' => '^5Conferma il tuo indirizzo email^6 per aggiungere un commento.',
+		'comment_must_login' => 'Fai il ^1log in^2 oppure ^3registrati^4 per aggiungere un commento.',
+		'comment_on_a' => 'Sulla risposta: ',
+		'comment_on_q' => 'Sul quesito: ',
+		'comment_q_popup' => 'Aggiungi un commento a questa domanda',
+		'delete_a_popup' => 'Elimina questa risposta in modo permanente',
+		'delete_button' => 'cancella',
+		'delete_c_popup' => 'Cancella questo commento in modo permanente',
+		'delete_q_popup' => 'Cancella questa quesito in modo permanente',
+		'duplicate_content' => 'Il tuo contributo sembra essere un doppione.',
+		'edit_a_popup' => 'Modifica questa risposta',
+		'edit_a_title' => 'Modifica la risposta',
+		'edit_button' => 'modifica',
+		'edit_c_popup' => 'Modifica questo commento',
+		'edit_c_title' => 'Modifica il commento',
+		'edit_must_confirm' => '^5Conferma il tuo indirizzo email^6 per editare questo.',
+		'edit_must_login' => 'Fai il ^1log in^2 oppure ^3registrati^4 per editare questo.',
+		'edit_q_popup' => 'Modifica questo quesito',
+		'edit_q_title' => 'Modifica il quesito',
+		'example_tags' => 'Tag di esempio: ',
+		'follow_a_popup' => 'Fai una domanda relativa a questa risposta',
+		'follow_button' => 'fai una domanda attinente',
+		'follows_a' => 'correlato alla risposta per:',
+		'follows_q' => 'relativo alla domanda:',
+		'hide_a_popup' => 'Nascondi questa risposta',
+		'hide_button' => 'nascondi',
+		'hide_c_popup' => 'Nascondi questo commento',
+		'hide_q_popup' => 'Nascondi questa domanda',
+		'matching_tags' => 'Tag corrispondenti: ',
+		'min_tags_x' => 'Inserire almeno ^ Tag',
+		'max_tags_x' => 'E\' consentito un massimo di ^ Tag',
+		'notify_email_note' => 'Privacy: Il tuo indirizzo e-mail sarà utilizzato esclusivamente per l\'invio di queste notifiche.',
+		'q_category_label' => 'Argomento:',
+		'q_content_label' => 'Ulteriori dettagli sulla tua domanda:',
+		'q_hidden_author' => 'Questa domanda è stata nascosta dal suo autore',
+		'q_hidden_other' => 'Questa domanda è stata nascosta',
+		'q_notify_email' => 'Inviami una email al seguente indirizzo se al mio quesito viene data una risposta o se viene commentato:',
+		'q_notify_label' => 'Inviami una email se al mio quesito viene data una risposta o se viene commentato',
+		'q_notify_x_label' => 'Inviami una email (^) se al mio quesito viene data una risposta o se viene commentato',
+		'q_tags_label' => 'Tag - usa i trattini per combinare più parole insieme:',
+		'q_title_label' => 'La tua domanda (sarà usata come titolo):',
+		'reply_button' => 'rispondi',
+		'reply_c_popup' => 'Rispondi a questo commento',
+		'reshow_button' => 'mostra di nuovo',
+		'select_popup' => 'Seleziona come miglior risposta',
+		'select_text' => 'Risposta migliore',
+		'unselect_popup' => 'Clicca per rimuovere la risposta migliore',
+		'x_answers_title' => '^ Risposte',
+		'your_answer_title' => 'La tua risposta',
+		'your_comment_a' => 'Il tuo commento a questa risposta:',
+		'your_comment_q' => 'Il tuo commento a questa domanda:',
+		'a_waiting_your_approval' => 'Questa risposta è in attesa della tua approvazione',
+		'a_your_waiting_approval' => 'La tua risposta sarà visionata ed approvata a breve.',
+		'add_q_favorites' => "Aggiungi questa domanda ai preferiti",
+		'approve_button' => "approvato",
+		'c_waiting_your_approval' => "Questo commento è in attesa della tua approvazione",
+		'c_your_waiting_approval' => "Il tuo commento sarà visionato ed approvato a breve.",
+		'category_js_note' => "Per selezionare una categoria, abilita Javascript sul tuo browser.",
+		'category_required' => "Seleziona una categoria",
+		'clear_flags_button' => "Rimuovi segnalazioni",
+		'clear_flags_popup' => "Rimuovi segnalazioni da tutti gli utenti",
+		'close_button' => "chiudi",
+		'close_duplicate_error' => "Il duplicato della domanda non può essere trovato - riprova inserendo il numero di un'altra domanda dall'URL, es. 123.",
+		'close_duplicate' => "Questo è un doppione di un'altra domanda",
+		'close_form_button' => "Chiudi la domanda",
+		'close_form_title' => "Chiudi la domanda",
+		'close_original_note' => "Puoi aggiungere il numero della domanda dall'URL, es. 123.",
+		'close_original_title' => "URL della domanda originale:",
+		'close_q_popup' => "Chiudi questa domanda (impossibile inserire nuove risposte)",
+		'close_reason_title' => "Motivo di chiusura della domanda:",
+		'closed_as_duplicate' => "chiuso come duplicato di:",
+		'closed_with_note' => "chiuso con la nota:",
+		'flag_a_popup' => "Marca questa risposta come spam o inappropriata",
+		'flag_button' => "Seleziona",
+		'flag_c_popup' => "Marca questo commento come spam o inappropriato",
+		'flag_hide_button' => "Seleziona e nascondi",
+		'flag_limit' => "Troppi messaggi selezionati - riprova più tardi.",
+		'flag_must_confirm' => "^5conferma il tuo indirizzo email^6 per selezionare i messaggi.",
+		'flag_must_login' => "^1log in^2 o ^3registrati^4 per selezionare i messaggi.",
+		'flag_not_allowed' => "Marca come non permessa",
+		'flag_q_popup' => "Marca questa domanda come spam o inappropriata",
+		'q_hidden_flagged' => "Questa domanda è stata selezionata e nascosta",
+		'q_tags_comma_label' => "Tag, utilizza la virgola come separatore:",
+		'q_waiting_approval' => "Questa domanda è in attesa di approvazione",
+		'q_waiting_your_approval' => "Questa domanda è in attesa della tua approvazione",
+		'q_your_waiting_approval' => "La tua domanda sarà verificata ed approvata a breve.",
+		'recat_button' => "Modifica categoria",
+		'recat_popup' => "Modifica la categoria di questa domanda",
+		'recat_q_title' => "Reimposta categoria alla domanda",
+		'reject_button' => "Respingi",
+		'remove_q_favorites' => "Togli questa domanda dai preferiti",
+		'reopen_button' => "Riapri",
+		'retag_button' => "Ritagga",
+		'retag_cat_popup' => "Modifica la categoria o i tag",
+		'retag_popup' => "Modifica i tag della domanda",
+		'retag_q_title' => "Ritagga domanda",
+		'show_1_comment' => "Mostra 1 commento",
+		'show_1_previous_comment' => "Mostra 1 commento precedente",
+		'show_x_comments' => "Visualizza ^ commenti",
+		'show_x_previous_comments' => "Mostra ^ commenti precedenti",
+		'unflag_button' => "Rimuovi la segnalazione",
+		'unflag_popup' => "Rimuovi la segnalazione",
+		'anon_name_label' => 'Il nome che vuoi visualizzare (opzionale):',		
+		'answer_must_be_approved' => 'Il tuo account deve essere approvato per poter inserire una risposta. Attendi o ^1aggiungi maggiori informazioni^2.',	
+		'ask_must_be_approved' => 'Il tuo account deve essere approvato per poter inserire una domanda. Attendi o ^1aggiungi maggiori informazioni^2.',
+		'category_ask_not_allowed' => 'Non disponi dei permessi per fare una domanda in questa categoria',	
+		'comment_must_be_approved' => 'Il tuo account deve essere approvato per poter inserire un commento. Attendi o ^1aggiungi maggiori informazioni^2.',
+		'comment_must_be_approved' => "Your account must be approved before you add a comment. Please wait or ^1add more information^2.",
+		'save_silent_label' => 'Salva senza notificare la modifica',
+		'approve_a_popup' => 'Approva questa risposta',
+		'approve_c_popup' => 'Approva questo commento',
+		'approve_q_popup' => 'Approva questa domanda',
+		'claim_a_popup' => 'Assegna questa risposta al tuo account utente',
+		'claim_c_popup' => 'Assegna questo commento al tuo account utente',
+		'claim_q_popup' => 'Assegna questa domanda al tuo account utente',
+		'reject_a_popup' => 'Respingi questa risposta',
+		'reject_c_popup' => 'Respingi questo commento',
+		'reject_q_popup' => 'Respingi questa domanda',
+		'reopen_q_popup' => 'Riapri questa domanda',
+		'reshow_a_popup' => 'Mostra nuovamente questa risposta',
+		'reshow_c_popup' => 'Mostra nuovamente questo commento',
+		'reshow_q_popup' => 'Mostra nuovamente questa domanda',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
