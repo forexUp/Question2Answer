9cc2b82358029c96e71274bde23b71584a6c70ab
forexUp <36788649+forexUp@users.noreply.github.com>
2018-02-24 18:47:26 +0100

    Create qa-lang-misc.php

---
+++ Q2A_1.8/qa-lang/it/qa-lang-misc.php
@@ -0,0 +1,137 @@
+<?php
+	
+/*
+	Question2Answer 1.2.1 (c) 2010, Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-include/qa-lang-misc.php
+	Version: 1.2.1
+	Date: 2010-07-29 03:54:35 GMT
+	Description: Miscellaneous language phrases
+
+
+	This software is free to use and modify for public websites, so long as a
+	link to http://www.question2answer.org/ is displayed on each page. It may
+	not be redistributed or resold, nor may any works derived from it.
+	
+	More about this license: http://www.question2answer.org/license.php
+
+
+	THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+	THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+	TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+	return array(
+		'block_ip_button' => "Blocca l'indirizzo IP",
+		'browse_categories' => 'Scegli la categoria',
+		'captcha_approve_fix' => 'Questa verifica non ti verrà più richiesta una volta che il tuo account sarà approvato.',
+		'captcha_confirm_fix' => "Per evitare questa verifica in futuro, ^5conferma il tuo indirizzo email^6.",
+		'captcha_error' => "Completare la verifica anti-spam!",
+		'captcha_label' => "Verifica anti-spam:",
+		'captcha_login_fix' => "Per evitare questa verifica in futuro, fai il ^1log in^2 oppure ^3registrati^4.",
+		'feed_a_edited_prefix' => 'Risposta modificata: ',		
+		'feed_a_prefix' => "Con risposte: ",
+		'feed_a_reshown_prefix' => 'Risposte modificate: ',		
+		'feed_a_selected_prefix' => 'Risposta selezionata: ',	
+		'feed_c_edited_prefix' => 'Commento modificato: ',
+		'feed_c_moved_prefix' => 'Commento spostato: ',		
+		'feed_c_prefix' => "Commentati: ",
+		'feed_c_reshown_prefix' => 'Commenti modificati: ',		
+		'feed_closed_prefix' => 'Chiuso: ',
+		'feed_edited_prefix' => 'Modificato: ',
+		'feed_hidden_prefix' => 'Nascosto: ',		
+		'feed_not_found' => "Commento non trovato",
+		'feed_recategorized_prefix' => 'Modificato categoria: ',
+		'feed_reopened_prefix' => 'Riaperto: ',
+		'feed_reshown_prefix' => 'Modificato: ',
+		'feed_retagged_prefix' => 'Ri-taggato: ',		
+		'feedback_email' => "La tua email (opzionale):",
+		'feedback_empty' => "Utilizza questo campo per inviare commenti e suggerimenti",
+		'feedback_message' => "I tuoi commenti e suggerimenti per ^:",
+		'feedback_name' => "Il tuo nome (opzionale):",
+		'feedback_sent' => "Il tuo messaggio sottostante è stato inviato - grazie!",
+		'feedback_title' => "Invia feedback",
+		'form_security_again' => 'Clicca nuovamente per confermare',
+		'form_security_reload' => 'Ricarica la pagina e prova di nuovo',
+		'hide_all_ip_button' => 'Nascondi tutti i messaggi provenienti da questo indirizzo IP',		
+		'host_name' => "Nome dell'host:",
+		'matches_blocked_ips' => "corrisponde ad uno degli indirizzi IP bloccati:",
+		'message_empty' => 'Inserisci il testo da inviare all\'utente',
+		'message_explanation' => 'Invieremo una notifica da ^. Il tuo indirizzo email non sarà visualizzato a meno che tu non lo inserisca nel messaggio.',
+		'message_for_x' => 'Il tuo messaggio per ^:',
+		'message_limit' => 'Non puoi inviare altri messaggi privati in quest\'ora',
+		'message_must_login' => '^1log in^2 o ^3registrati^4 per inviare messaggi privati.',
+		'message_recent_history' => 'Messaggi recenti con ^',
+		'message_sent' => 'Il tuo messaggio privato è stato inviato',
+		'my_favorites_title' => 'Preferiti',
+		'nav_all_my_updates' => 'Tutti i miei aggiornamenti',
+		'nav_my_content' => 'I miei contenuti',
+		'nav_my_details' => 'I miei dettagli',
+		'nav_my_favorites' => 'I miei preferiti',
+		'nav_user_activity' => 'Attività recente',
+		'nav_user_as' => 'Tutte le risposte',
+		'nav_user_qs' => 'Tutte le domande',
+		'nav_user_wall' => 'Bacheca',
+		'no_activity_from_x' => 'Nessuna attività da ^',
+		'no_favorite_categories' => 'Nessuna categoria preferita',
+		'no_favorite_qs' => 'Nessuna domanda preferita',
+		'no_favorite_tags' => 'Nessun tag preferito',
+		'no_favorite_users' => 'Nessun utente preferito',
+		'no_recent_updates' => 'Nessun aggiornamento recente',
+		'no_updates_content' => 'Nessun aggiornamento recente sui miei contenuti',
+		'no_updates_favorites' => 'Nessun aggiornamento tra i miei preferiti',
+		'private_message_title' => 'Invia un messaggio privato',
+		'recent_activity_from_x' => 'Attività recente da ^',
+		'recent_updates_content' => 'Aggiornamenti recenti sui miei contenuti',
+		'recent_updates_favorites' => 'Aggiornamenti recenti sui miei preferiti',
+		'recent_updates_title' => 'Aggiornamenti recenti per me',
+		'site_in_maintenance' => 'Il sito è in manutenzione, torna a trovarci presto.',
+		'suggest_favorites_add' => 'Per aggiungere una domanda o altro ai tuoi preferiti, clicca il ^ in alto in questa pagina.',
+		'suggest_update_favorites' => 'Per ulteriori aggiornamenti, aggiungi l\'oggetto ^1ai tuoi preferiti^2.',		
+		'unblock_ip_button' => "Sblocca indirizzi IP",
+		'your_a_commented' => 'commento alla tua risposta',
+		'your_a_edited' => 'modifica alla tua risposta',
+		'your_a_hidden' => 'la tua risposta è stata nascosta',
+		'your_a_questioned' => 'domandato sulla tua risposta',
+		'your_a_reshown' => 'la tua risposta rivista',
+		'your_a_selected' => 'la tua risposta selezionata',
+		'your_c_edited' => 'il tuo commento modificato',
+		'your_c_followed' => 'il tuo commento successivo',
+		'your_c_hidden' => 'il tuo commento nascosto',
+		'your_c_moved' => 'il tuo commento spostato',
+		'your_c_reshown' => 'il tuo commento rivisto',
+		'your_q_answered' => 'la tua domanda risposta',
+		'your_q_closed' => 'la tua domanda è chiusa',
+		'your_q_commented' => 'la tua domanda commentata',
+		'your_q_edited' => 'la tua domanda modificata',
+		'your_q_hidden' => 'la tua domanda nascosta',
+		'your_q_recategorized' => 'modifica alla categoria della tua domanda',
+		'your_q_reopened' => 'la tua domanda riaperta',
+		'your_q_reshown' => 'la tua domanda rivista',
+		'your_q_retagged' => 'la tua risposta ri-taggata',
+		'inbox' => 'Messaggi',	
+		'more_favorite_qs' => 'Altre domande preferite...',
+		'more_favorite_tags' => 'Altri tag preferiti...',
+		'more_favorite_users' => 'Altri utenti preferiti...',
+		'nav_user_pms' => 'Messaggi privati',
+		'outbox' => 'Messaggi inviati',
+		'pm_inbox_title' => 'Messaggi privati ricevuti',
+		'next_step' => "Prossimo step",		
+		'pm_outbox_title' => 'Messaggi privati inviati'
+	);
+
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
